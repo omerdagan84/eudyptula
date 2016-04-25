cbc3b8eeaf3eb1d157685d49bf38aaa113d2d550
Ramon Fried <ramon.fried@gmail.com>
2016-04-25 15:48:20 +0300

    Task 08: add jiffies file

--- task8/task8.c
+++ task8/task8.c
@@ -21,6 +21,7 @@
 #include <linux/uaccess.h>
 #include <linux/fs.h>
 #include <linux/debugfs.h>
+#include <linux/jiffies.h>
 
 #define MAX_INPUT_SIZE 20
 static struct dentry *debug_dir = NULL;
@@ -68,6 +69,32 @@ static const struct file_operations task8_fops = {
 	.write	= task8_write,
 };
 
+
+static ssize_t jiffies_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+
+	char ret_string[25];
+	int str_size;
+	str_size = sprintf(ret_string, "%lu", jiffies);	
+
+	if (*ppos == str_size)
+		return 0;
+	else
+		if (*ppos != 0 || count < str_size)
+			return -EINVAL;
+
+	if (copy_to_user(buf, ret_string, str_size))
+		return -EINVAL;
+
+	*ppos = str_size;
+	return *ppos;
+}
+static const struct file_operations jiffies_fops = {
+	.owner	= THIS_MODULE,
+	.read	= jiffies_read,
+	.write	= NULL,
+};
+
 static int __init task8_init(void)
 {
 	/*create the debugfs directory*/
@@ -78,6 +105,10 @@ static int __init task8_init(void)
 	}
 
 	debug_id = debugfs_create_file("id", 0666, debug_dir, NULL, &task8_fops);
+	if (!debug_id)
+		goto cleanup_debugfs;
+	
+	debug_id = debugfs_create_file("jiffies", 0444, debug_dir, NULL, &jiffies_fops);
 	if (!debug_id)
 		goto cleanup_debugfs;
 
